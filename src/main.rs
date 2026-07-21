b7f0c387a0f7c598becfbb6096f478c7a51613b4
OkCom <90555600+1156666@users.noreply.github.com>
2026-07-21 20:38:15 +0800

    this fucking splite_with_whitespace is junk

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 pub mod lex;
 pub mod par;
 
-
-
+use crate::lex::lexer::Lexer;
 
 fn main() {
-    println!("Hello, world!");
+    let mut lexer = Lexer::new();
+    lexer.token_pattern("1 + 2 = 5");
+    lexer.print()
 }

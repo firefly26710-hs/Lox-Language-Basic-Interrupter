ade401ad33a9f52476171619d31f8b2e4b6dd61e
OkCom <90555600+1156666@users.noreply.github.com>
2026-08-01 14:24:41 +0800

    r

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::collections::HashMap;
+use std::collections::HashMap;
 use std::io;
 use std::io::Write;
+use crate::lex::lexer::Lexer;
+use crate::par::parser::SyntaxTree;
+
+pub mod lex;
+pub mod par;
+
 fn main() {
+    let mut var_table:HashMap<String, f64> = HashMap::new();
     loop{
         print!(">> ");
         io::stdout().flush().unwrap();
@@ -10,7 +18,26 @@ fn main() {
             io::stdin().read_line(&mut buf).unwrap();
             buf
         };
+        if input.trim() == "exit"{
             break;
         }
-        }
+        let mut lexer = Lexer::new(&input);
+        lexer.scan_tokens();
+        println!("tokens {:?}", &lexer.tokens);
+        let mut tree = SyntaxTree::new(lexer.tokens);
+        tree.parser_expression(0.0);
+        let start_index = tree.nodes.len() - 1;
+        let result = tree.eval(start_index, &mut var_table);
+        tree.print();
+        println!("Result : {}", result);
 
+    }
+
+
+
+
+
+
+
+
+}
--- target/debug/deps/cpl-269f64b61f6c88c5.d
+++ target/debug/deps/cpl-269f64b61f6c88c5.d
@@ -1,11 +1,5 @@
-/home/fireflylover/rr/cpl/target/debug/deps/cpl-269f64b61f6c88c5.d: src/main.rs src/lex/mod.rs src/lex/token.rs src/lex/lexer.rs src/par/mod.rs src/par/parser.rs src/par/node.rs
+/home/fireflylover/rr/cpl/target/debug/deps/cpl-269f64b61f6c88c5.d: src/main.rs
 
-/home/fireflylover/rr/cpl/target/debug/deps/libcpl-269f64b61f6c88c5.rmeta: src/main.rs src/lex/mod.rs src/lex/token.rs src/lex/lexer.rs src/par/mod.rs src/par/parser.rs src/par/node.rs
+/home/fireflylover/rr/cpl/target/debug/deps/libcpl-269f64b61f6c88c5.rmeta: src/main.rs
 
 src/main.rs:
-src/lex/mod.rs:
-src/lex/token.rs:
-src/lex/lexer.rs:
-src/par/mod.rs:
-src/par/parser.rs:
-src/par/node.rs:
